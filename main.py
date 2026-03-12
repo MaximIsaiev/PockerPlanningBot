35637c6b6cceea7cb1cb36ad07500344057303bd
whendigo <maksim.isaiev.02@gmail.com>
2026-03-12 21:24:35 +0300

    [*] add echo logic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import logging
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()
 
@@ -13,8 +13,16 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!"
+    )
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text
+    )
 
 if __name__ == '__main__':
     token = os.getenv('BOT_TOKEN')
@@ -24,6 +32,8 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
 
     application.run_polling()
